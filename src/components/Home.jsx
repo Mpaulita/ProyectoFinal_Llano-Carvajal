78c656b3d1e0e7d7c95b8e596d83a4afac9a5872
JuDaCa-UAO <juan_david.carvajal@uao.edu.co>
2024-11-04 13:48:24 -0500

    Comienzos en la creación del componente Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React from 'react'; 
 import '../Styles/inicio.css';
 
 const Home = () => {
   return (
     <div className="inicio-contenedor">
-      <h1 className="inicio-header">Bienvenido a SkillSwap</h1>
-      <p className="inicio-descripcion">Intercambia habilidades, aprende nuevas destrezas y conecta con expertos de todo el mundo. ¡Únete ahora y comienza tu viaje de aprendizaje!</p>
-      {/* <a href="/" className="inicio-boton">¡Únete a SkillSwap!</a> */}
+      <div className="header-container">
+        <h1 className="inicio-header">Bienvenido a SkillSwap</h1>
+        <p className="inicio-descripcion">
+          Intercambia habilidades, aprende nuevas destrezas y conecta con expertos de todo el mundo. ¡Únete ahora y comienza tu viaje de aprendizaje!
+        </p>
+      </div>
+      
+      <div className="horizontal-container">
+        <button className="button-swap"></button>
+        <button className="button-add"></button>
+        <button className="button-text"></button>
+        <button className="button-profile"></button>
+        <input type="text" placeholder="Buscar"/>
+        <button></button>
+      </div>
     </div>
   );
 };
