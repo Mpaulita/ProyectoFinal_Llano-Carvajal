2c4482cb81b676c61ee83ad3425f1c4517b9eb75
JuDaCa-UAO <juan_david.carvajal@uao.edu.co>
2024-11-04 21:21:14 -0500

    Arreglé los estilos de home y puse íconos para los botones

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -26,27 +26,117 @@ const Home = () => {
       </div>
 
       <div className="horizontal-container">
-        <button className="button-swap">
-          <i className="icon-swap" /> {/* Icono opcional */}
+        <div className="button-text-home">
+          <button className="button-swap">
+            <i className="icon-swap" />{" "}
+            {
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M7.5 21 3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5"
+                />
+              </svg>
+            }
+          </button>
           <span>Swap</span>
-        </button>
-        <button className="button-help">
-          <i className="icon-help" /> {/* Icono opcional */}
-          <span>Ayuda</span>
-        </button>
-        <button className="button-requests">
-          <i className="icon-requests" /> {/* Icono opcional */}
-          <span>Solicitudes</span>
-        </button>
-        <button className="button-profile">
-          <i className="icon-profile" /> {/* Icono opcional */}
+        </div>
+        <div className="button-text-home">
+          <button className="button-help">
+            <i className="icon-help" />{" "}
+            {
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 4.5v15m7.5-7.5h-15"
+                />
+              </svg>
+            }
+          </button>
+          <span>Publicar propuesta</span>
+        </div>
+        <div className="button-text-home">
+          <button className="button-requests">
+            <i className="icon-requests" />{" "}
+            {
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
+                />
+              </svg>
+            }
+          </button>
+          <span>Mensajes</span>
+        </div>
+        <div className="button-text-home">
+          <button className="button-profile">
+            <i className="icon-profile" />{" "}
+            {
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                />
+              </svg>
+            }
+          </button>
           <span>Perfil</span>
-        </button>
+        </div>
         <input type="text" placeholder="Buscar" />
-        <button className="button-logout">
-          <i className="icon-logout" /> {/* Icono opcional */}
+        <div className="button-text-home">
+          <button className="button-logout">
+            <i className="icon-logout" />{" "}
+            {
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15"
+                />
+              </svg>
+            }
+          </button>
           <span>Salir</span>
-        </button>
+        </div>
       </div>
 
       <div className="card-container">
